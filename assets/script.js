7eb5f173be1163d57e7bd7e0b5841d88519e3323
wingram1 <waingram96@gmail.com>
2022-01-26 17:25:42 -0600

    added function to retrieve forecast for specific coordinates

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,50 @@
-//testing some api's
+// gets denver city center
+var lat = 39.736762;
+var lon = -104.963855;
+
+var getForecast = function() {
+    var apiUrl = "https://api.weather.gov/points/" + lat + "," + lon;
+
+    //connect to weather.gov
+    fetch(apiUrl)
+        .then(function(response) {
+            if (response.ok) {
+                console.log("Connection made successfully");
+
+                response.json().then(function(data) {
+                    // log data object
+                    console.log(data)
+                    console.log("Retrieving forcast for " + data.properties.relativeLocation.properties.city + ", " + data.properties.relativeLocation.properties.state + "...");
+
+                    // get forecast link 
+                    fetch(data.properties.forecast)
+                        .then(function(response) {
+                            console.log("Forcast retrieved!")
+                            console.log(data);
+
+                            // get seven-day forecast
+                            response.json().then(function(data) {
+                                var sevenDay = data.properties.periods;
+
+                                // log each array index
+                                for (i=0; i < sevenDay.length; i++) {
+                                console.log(sevenDay[i].name + ": " + sevenDay[i].shortForecast);
+                                }
+                            })
+                        })
+                        .catch(function(error) {
+                            console.log("Error! Unable to retrieve forecast");
+                        })
+                })
+            }
+            else {
+                console.log("Error connecting to weather.gov api");
+            }
+        })
+        .catch(function(error) {
+            console.log("Unable to connect to weather.gov");
+        })
+
+}
+
+getForecast();
