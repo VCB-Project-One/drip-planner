46be01fc2d7a2453e3ed5aa56cfa3eb8fd295a3b
wingram1 <waingram96@gmail.com>
2022-01-26 19:43:17 -0600

    added function to generate forecast elements to html

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,14 @@
+var forecastContainer = document.querySelector("#forecast-container");
+
 // gets denver city center
 var lat = 39.736762;
 var lon = -104.963855;
 
+
+// function to get forecast for given lat/lon
 var getForecast = function() {
+
+    // set api URL
     var apiUrl = "https://api.weather.gov/points/" + lat + "," + lon;
 
     //connect to weather.gov
@@ -19,17 +25,28 @@ var getForecast = function() {
                     // get forecast link 
                     fetch(data.properties.forecast)
                         .then(function(response) {
-                            console.log("Forcast retrieved!")
+                            console.log("Forecast retrieved!")
                             console.log(data);
 
                             // get seven-day forecast
                             response.json().then(function(data) {
                                 var sevenDay = data.properties.periods;
+                                var forecastArray = [];
 
                                 // log each array index
                                 for (i=0; i < sevenDay.length; i++) {
-                                console.log(sevenDay[i].name + ": " + sevenDay[i].shortForecast);
+                                    // creates objects using forecast data
+                                    var tempObject = {
+                                        day: sevenDay[i].name,
+                                        shortForecast: sevenDay[i].shortForecast
+                                    };
+
+                                    // push tempObject to forecastArray
+                                    forecastArray.push(tempObject);
                                 }
+
+                                //generate HTML
+                                generateForecast(forecastArray)
                             })
                         })
                         .catch(function(error) {
@@ -47,4 +64,33 @@ var getForecast = function() {
 
 }
 
+var generateForecast = function(array) {
+    // convert array from JSON object to string
+    JSON.stringify(array);
+
+    for (i=0; i<array.length; i++) {
+        console.log("Working array: " + JSON.stringify(array[i]) );
+
+        // create container for individual forecast
+        var forecastEl = document.createElement("div");
+        forecastEl.className = "";
+        forecastEl.id = "";
+        forecastContainer.appendChild(forecastEl);
+
+        var dayName = document.createElement("h3");
+        dayName.className = "";
+        dayName.textContent = array[i].day;
+        forecastEl.appendChild(dayName);
+
+        var dayDetails = document.createElement("p");
+        dayDetails.className = "";
+        dayDetails.textContent = array[i].shortForecast;
+        forecastEl.appendChild(dayDetails);
+    }
+}
+
+
+
+
+/////////////////// CALL FUNCTION //////////////////
 getForecast();
